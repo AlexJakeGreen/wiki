8fc533377fa0ec009ecb7367d9eab528be5ae330
Oleksandr Litvinov <alexanderjakegreen@gmail.com>
2020-04-19 04:42:28 +0300

    Add a new url type

--- wiki.el
+++ wiki.el
@@ -1,52 +1,61 @@
 ;;; wiki.el -- Wiki package
-;;; Code:
 ;;; Commentary:
+;;; Code:
+
+(require 'org)
 
 (defvar wiki-dir "~/org/wiki/")
 
-(defun wiki-get-page-path (pagename)
-  "Return file path for PAGENAME."
-  (concat wiki-dir "/" pagename ".org"))
+(defun wiki--page-path (name)
+  "Return file path for page NAME."
+  (concat wiki-dir "/" name ".org"))
 
-(defun wiki-get-pagename-from-path (path)
-  "Return pagename from PATH."
-  (substring path (+ 1 (length (expand-file-name wiki-dir))) (length path)))
+(defun wiki--dirname (name)
+  "Return dirname for page NAME."
+  (replace-regexp-in-string "[^/]+$" "" (wiki--page-path name)))
 
-(defun wiki-page-exists (pagename)
-  "Check if PAGENAME exists."
-  (file-exists-p (wiki-get-page-path pagename)))
+(defun wiki--page-exists (name)
+  "Check if page NAME exists."
+  (file-exists-p (wiki--page-path name)))
 
-(defun wiki-dirname (pagename)
-  "Return dirname for PAGENAME."
-  (replace-regexp-in-string "[^/]+$" "" (wiki-get-page-path pagename)))
+(defun wiki--make-link (name)
+  "Make a link from page NAME."
+  (format "[[wiki:%s][%s]]"
+          name
+          (replace-regexp-in-string ".+/\\([^/]+\\)$" "\\1" name)))
 
-(defun wiki-open-page (pagename)
-  "Open wiki page PAGENAME."
+(defun wiki-open (name)
+  "Open wiki page NAME."
   (interactive)
-  (if (wiki-page-exists pagename)
-      (find-file (wiki-get-page-path pagename))
+  (if (wiki--page-exists name)
+      (find-file (wiki--page-path name))
     (progn
-      (unless (file-directory-p (wiki-dirname pagename))
-        (make-directory (wiki-dirname pagename) t))
-      (find-file (wiki-get-page-path pagename))
+      (unless (file-directory-p (wiki--dirname name))
+        (make-directory (wiki--dirname name) t))
+      (find-file (wiki--page-path name))
       (insert (format (concat
                        "#+TITLE: %s\n"
                        "#+DESCRIPTION:\n"
+                       "#+TIMESTAMP: %s\n"
                        "#+STARTUP:  content\n"
                        "\n\n"
-                       "- [[index][Index]]\n\n")
-                      pagename))
+                       "- [[wiki:index][Index Page]]\n\n")
+                      name
+                      (current-time-string)))
       (save-buffer)
       )))
-
-(defun wiki-make-link (pagename)
-  "Make a link from PAGENAME."
-  (format "[[%s][%s]]" pagename pagename))
 
 (defun wiki-insert-link ()
   "Insert a link to a page."
   (interactive)
-  (insert (wiki-make-link (read-string "Enter link path: "))))
+  (insert (wiki--make-link (read-string "Enter link path: "))))
+
+(defun wiki--link-follow (path)
+  "Follow a link with PATH."
+  (wiki-open path))
+
+(org-link-set-parameters "wiki"
+                         :follow 'wiki-open)
 
 (provide 'wiki)
 ;;; wiki.el ends here
